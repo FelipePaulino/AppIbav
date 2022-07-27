e17c96606776cd7a1904c2b1c8cabb8ca5ef9ea6
DuzaoOliveira <edu.oliblack@hotmail.com>
2022-07-27 18:51:17 -0300

    aplicando disabled nos selects

--- src/screens/Members/index.tsx
+++ src/screens/Members/index.tsx
@@ -121,11 +121,11 @@ export function MembersScreen(this: any) {
     });
     dispatch({
       type: FormReportActions.setDiscipuladoSelect,
-      payload: null,
+      payload: 'Selecione',
     });
     dispatch({
       type: FormReportActions.setCelulaSelect,
-      payload: null,
+      payload: 'Selecione',
     });
   };
 
@@ -136,7 +136,7 @@ export function MembersScreen(this: any) {
     });
     dispatch({
       type: FormReportActions.setCelulaSelect,
-      payload: null,
+      payload: 'Selecione',
     });
   };
 
@@ -338,7 +338,7 @@ export function MembersScreen(this: any) {
                 <SelectComponent
                   onChange={(handleDiscipuladoChange)}
                   labelSelect={state.discipuladoSelect}
-                  dataOptions={mapDiscipuladosUnicos}
+                  dataOptions={state.redeSelect && mapDiscipuladosUnicos}
                   selectedOption={handleDiscipuladoChange}
                   width='300'
                   disabled={state.redeSelect === "Selecione" ? true : false}
