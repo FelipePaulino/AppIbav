88a2611c5110391aa319bbd602900972d0e9d893
DuzaoOliveira <edu.oliblack@hotmail.com>
2022-11-17 21:53:22 -0300

    ajustando tela multiplicação discipulado

--- src/screens/MutiplicationDiscipulado/index.tsx
+++ src/screens/MutiplicationDiscipulado/index.tsx
@@ -3,11 +3,52 @@ import { ScrollView } from "react-native";
 import MenuNavigation from "../../common/constants/navigation";
 import { ComeBackComponent } from "../../components/ComeBack";
 import { HeaderComponent } from "../../components/Header";
+import { SelectComponent } from "../../components/Select";
 import { TitleComponent } from "../../components/Title";
+import { useFormReport } from "../../hooks/useFormReport";
+import { FormReportActions } from "../../contexts/FormReport";
 
 import * as S from "./styles";
 
 export function MultiplicationDiscipulado() {
+  const { state, dispatch } = useFormReport();
+
+  const handleRedeChange = (value: string) => {
+    dispatch({
+      type: FormReportActions.setRedeSelect,
+      payload: value,
+    });
+    dispatch({
+      type: FormReportActions.setDiscipuladoSelect,
+      payload: "Selecione",
+    });
+    dispatch({
+      type: FormReportActions.setCelulaSelect,
+      payload: "Selecione",
+    });
+  };
+
+  const handleDiscipuladoChange = (value: string) => {
+    dispatch({
+      type: FormReportActions.setDiscipuladoSelect,
+      payload: value,
+    });
+    dispatch({
+      type: FormReportActions.setCelulaSelect,
+      payload: "Selecione",
+    });
+  };
+
+  const handleCelulaChange = (value: string) => {
+    dispatch({
+      type: FormReportActions.setCelulaSelect,
+      payload: value,
+    });
+  };
+
+
+  const mockRedes = [{ value: '1' }, { value: '2' }, { value: '3' }]
+
   return (
     <>
       <HeaderComponent>
@@ -22,7 +63,41 @@ export function MultiplicationDiscipulado() {
             <TitleComponent title={`Rede`} small primary />
             <S.ContentC>
               <S.IconC name="vector-square" />
-
+              <SelectComponent
+                onChange={handleRedeChange}
+                labelSelect={'Mock'}
+                dataOptions={mockRedes}
+                selectedOption={handleRedeChange}
+                width="300"
+              />
+            </S.ContentC>
+          </S.Grid>
+          <S.Grid>
+            <TitleComponent title={`Discipulado Atual:`} small primary />
+            <S.ContentC>
+              <S.IconC name="network-wired" />
+              <SelectComponent
+                onChange={handleDiscipuladoChange}
+                labelSelect={'mock'}
+                dataOptions={mockRedes}
+                selectedOption={handleDiscipuladoChange}
+                width="300"
+                disabled={state.redeSelect === "Selecione" ? true : false}
+              />
+            </S.ContentC>
+          </S.Grid>
+          <S.Grid>
+            <TitleComponent title={`Celula`} small primary />
+            <S.ContentC>
+              <S.IconC name="user-friends" />
+              <SelectComponent
+                onChange={handleCelulaChange}
+                labelSelect={'mock'}
+                dataOptions={mockRedes}
+                selectedOption={handleCelulaChange}
+                width="300"
+                disabled={state.discipuladoSelect === "Selecione" ? true : false}
+              />
             </S.ContentC>
           </S.Grid>
         </S.Content>
